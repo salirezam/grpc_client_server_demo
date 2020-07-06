dc053404a253f254abbd5741824864455f6ad110
Alireza <s.alireza.mohammadi@gmail.com>
2020-07-06 19:49:41 +0100

    Update server to support chat

--- server/main.go
+++ server/main.go
@@ -10,6 +10,8 @@ import (
 
 // run a gRPC
 func main() {
+	var connections []*api.Connection
+
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 7777))
 
 	if err != nil {
@@ -17,13 +19,16 @@ func main() {
 	}
 
 	// create a server
-	server := api.Server{}
+	server := api.Server{connections}
 
 	// create gRPC server
 	grpcServer := grpc.NewServer()
 
 	// attach the Greeting service to the server
 	api.RegisterGreetingServer(grpcServer, &server)
+
+	// attach the Broadcast service to the server
+	api.RegisterBroadcastServer(grpcServer, &server)
 
 	// start the server
 	if err := grpcServer.Serve(lis); err != nil {
